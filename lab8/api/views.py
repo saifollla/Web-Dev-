319a145ae302f31a5dfb94132e16672b4ec9c80b
zere <ajeresafiulla@gmail.com>
2026-04-03 15:07:00 +0500

    lab8

--- lab8/api/views.py
+++ lab8/api/views.py
@@ -1,9 +1,23 @@
 from django.http import JsonResponse
 from .models import Product, Category
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .serializers import CategorySerializer, ProductSerializer
 # Create your views here.
 
 def product_list(request):
     products = Product.objects.all()
+    category_id = request.GET.get('category')
+    active = request.GET.get('active')
+    search = request.GET.get('search')
+    if category_id:
+        products = products.filter(category_id=category_id)
+    if active is not None:
+        is_active = active.lower() == 'true'
+        products = products.filter(is_active=is_active)
+    if search:
+        products = products.filter(name__icontains=search)
     data = {'products': list(products.values())}
     return JsonResponse(data, safe=False)
 
@@ -41,3 +55,17 @@ def category_products(request, id):
         return JsonResponse(list(products.values()), safe=False)
     except Category.DoesNotExist:
         return JsonResponse({'error': 'Category not found'}, status=404)
+    
+class CategoryViewSet(viewsets.ModelViewSet):   
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    @action(detail=True, methods=['get'])
+    def products(self, request, pk=None):
+        category = self.get_object()
+        products = category.products.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+class ProductViewSet(viewsets.ModelViewSet):    
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
